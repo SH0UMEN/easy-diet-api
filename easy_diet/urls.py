61ca843fa5b1abc950098dd507d67dba02e5f623
shoumen <0showman0@gmail.com>
2022-07-03 11:43:22 +0300

    Add frontend view

--- easy_diet/urls.py
+++ easy_diet/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+
+def index_view(request):
+    return render(request, 'index.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', index_view)
 ]
