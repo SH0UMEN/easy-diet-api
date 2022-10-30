43ebc08e7c3c319a92f02dcfa93c56bac4d465c6
shoumen <0showman0@gmail.com>
2022-10-30 05:42:38 +0300

    Made author setting after record creation

--- food/models/dish.py
+++ food/models/dish.py
@@ -6,5 +6,11 @@ from .product import Product
 class Dish(models.Model):
 	products = models.ManyToManyField(Product)
 	title = models.CharField(max_length=30)
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
 	image = models.ImageField(upload_to='dish_images')
+
+	def save_model(self, request, obj, form, change):
+		if not obj.pk:
+			obj.author = request.user
+
+		super().save_model(request, obj, form, change)
--- food/models/menu.py
+++ food/models/menu.py
@@ -7,5 +7,11 @@ class Menu(models.Model):
 	dishes = models.ManyToManyField(Dish)
 	title = models.CharField(max_length=30)
 	parent = models.ForeignKey('self', on_delete=models.CASCADE)
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
 	image = models.ImageField(upload_to='menu_images')
+
+	def save_model(self, request, obj, form, change):
+		if not obj.pk:
+			obj.author = request.user
+
+		super().save_model(request, obj, form, change)
