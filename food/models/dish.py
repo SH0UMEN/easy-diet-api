68fe886bb41c5d71053edcfc9cabce4a38edadc8
shoumen <0showman0@gmail.com>
2022-12-15 07:50:36 +0300

    Made image required

--- food/models/dish.py
+++ food/models/dish.py
@@ -6,5 +6,5 @@ from .product import Product
 class Dish(models.Model):
 	dish_product_relations = models.ManyToManyField(Product, through='DishProduct', blank=True)
 	author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-	image = models.ImageField(upload_to='static/dish_images', null=True)
+	image = models.ImageField(upload_to='static/dish_images')
 	title = models.CharField(max_length=30)
