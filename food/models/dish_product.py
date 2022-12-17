bf0e0a658223a8d2540d7a7e567ad3fd4868baef
shoumen <0showman0@gmail.com>
2022-12-17 11:54:42 +0300

    Little fix

--- food/models/dish_product.py
+++ food/models/dish_product.py
@@ -4,6 +4,6 @@ from .dish import Dish
 
 
 class DishProduct(models.Model):
-	dish = models.ForeignKey(Dish, blank=True, on_delete=models.CASCADE)
+	dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
 	product = models.ForeignKey(Product, on_delete=models.CASCADE)
 	grams = models.IntegerField()
