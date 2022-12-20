7cc0fc9c4596c3cd2f2b9c4289c9d27b6c499b86
shoumen <0showman0@gmail.com>
2022-12-20 07:41:36 +0300

    Added description to dishes

--- api/views/dish.py
+++ api/views/dish.py
@@ -1,15 +1,19 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
-from food.models import Dish
+from rest_framework.filters import OrderingFilter
 from api.serializers.dish.dish import DishSerializer
 from api.permissions import IsOwnerOrReadOnly
+from food.models import Dish
 
 
 class DishesView(ListCreateAPIView):
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['creation_date']
     filterset_fields = ['author']
+    ordering = '-creation_date'
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
--- food/models/dish.py
+++ food/models/dish.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.utils import timezone
 from django.db import models
 from .product import Product
 
@@ -6,5 +7,7 @@ from .product import Product
 class Dish(models.Model):
 	dish_product_relations = models.ManyToManyField(Product, through='DishProduct', blank=True)
 	author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+	creation_date = models.DateTimeField(default=timezone.now)
+	description = models.TextField(max_length=300, default='')
 	image = models.ImageField(upload_to='dishes')
 	title = models.CharField(max_length=30)
