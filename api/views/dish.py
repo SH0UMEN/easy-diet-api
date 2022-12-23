7b6bd441959696e51835ce9e4f30c416c383b3d1
shoumen <0showman0@gmail.com>
2022-12-23 09:58:26 +0300

    Add filtering to Dish

--- api/views/dish.py
+++ api/views/dish.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.filters import OrderingFilter, SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from api.serializers.dish.dish import DishSerializer
 from api.permissions import IsOwnerOrAdminOrReadOnly
 from food.models import Dish
@@ -10,7 +11,7 @@ class DishesView(ListCreateAPIView):
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [OrderingFilter, SearchFilter]
+    filter_backends = [OrderingFilter, DjangoFilterBackend, SearchFilter]
     ordering_fields = ['creation_date']
     filterset_fields = ['author']
     ordering = '-creation_date'
