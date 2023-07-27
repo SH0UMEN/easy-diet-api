57d9408e9d72a46baf94e8e50c0b8c16009ea5d4
shoumen <0showman0@gmail.com>
2023-07-27 09:09:17 +0700

    User code fix + ui fixes

--- food/models/user.py
+++ food/models/user.py
@@ -3,7 +3,7 @@ from django.db.models import CharField
 
 
 class User(AbstractUser):
-    code = CharField(max_length=32, blank=True, null=True)
+    code = CharField(max_length=32, default=None, blank=True, null=True)
 
     class Meta:
         db_table = 'auth_user'
