e9962c496a57f8277bc00221681f57e3bad0ebed
shoumen <0showman0@gmail.com>
2023-10-23 01:47:27 +0700

    Fixed asgi
    (cherry picked from commit 0688893d0eb2308aaf383e4db2783a2c7955a3ef)

--- easy_diet/asgi.py
+++ easy_diet/asgi.py
@@ -13,12 +13,14 @@ from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from api.urls import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easy_diet.settings')
+django_asgi_app = get_asgi_application()
+
+from api.urls import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-	"http": get_asgi_application(),
+	"http": django_asgi_app,
 	"websocket": AllowedHostsOriginValidator(
 		AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
 	),
